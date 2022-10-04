a721b0c20ada932343187259b37025970563cd5c
“night” <eternalnight1226@qq.com>
2022-10-05 00:40:44 +0800

    Site updated: 2022-10-05 00:40:43

---
+++ js/disable.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+  document.onkeydown = function () {
+      if (window.event.keyCode == 123) {
+          alert("你知道的也太多了吧，回去好好休息吧！");
+          window.location.href="https://www.nightsoil.cn";
+          event.keyCode = 0;
+          event.returnValue = false;
+      }
+ 	if (window.event == 123) {
+          alert("你知道的也太多了吧，回去好好休息吧！");
+          window.location.href="https://www.nightsoil.cn";
+          event.keyCode = 0;
+          event.returnValue = false;
+      }
+  }
+</script>
